e51c087c4e06802bbd4a213ba937374611fcd7ee
Satawat <satawatboykrathok@gmail.com>
2024-01-24 15:32:23 +0700

    edit add delete

--- index.js
+++ index.js
@@ -56,6 +56,87 @@ app.get('/car_list',(req,res)=>{
 
 
 
+app.get("/edit_employee/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("SELECT * FROM employee WHERE id_employee = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+
+app.put("/update_emp", (req, res) => { 
+  const first_name = req.body.first_name; 
+  const last_name = req.body.last_name;
+  const department = req.body.department;
+  const id_employee = req.body.id_employee;
+  //const res_id = req.body.res_id;
+  db.query(
+    "UPDATE employee SET first_name = ?,last_name = ?,department = ? WHERE id_employee = ?",
+    [first_name, last_name, department, id_employee],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+
+app.post('/add_emp', (req, res) => {
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const department = req.body.department;
+ 
+ 
+
+  db.query(
+    "INSERT INTO employee (first_name, last_name , department) VALUES (?,?,?)",
+    [first_name, last_name, department],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        //console.log("result")
+        //console.log(result)
+
+        res.send(result);
+      }
+    }
+  );
+});
+
+
+app.get('/emp_history',(req,res)=>{
+  db.query("SELECT * FROM employee", (err,result)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+
+app.delete("/delete_emp/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM employee WHERE id_employee = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("yes");
+    }
+  });
+});
+
+
+
 
 
 
